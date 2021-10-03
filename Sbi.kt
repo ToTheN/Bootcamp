b6b9f7cfc332a6fdc3b3846ce459c40bd9c1b400
Tushar <90312565+ToTheN@users.noreply.github.com>
2021-10-03 16:56:30 +0530

    Delete Sbi.kt

--- Sbi.kt
+++
@@ -1,9 +0,0 @@
-open class Sbi {
-    open fun getDetails(){
-       var branchName:String = "sbi"
-        var rointerest:Int = 4
-
-        println("branchName =  $branchName")
-        println("rointerest = $rointerest")
-    }
-}
